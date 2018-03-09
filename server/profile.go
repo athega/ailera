ff5ed751e6474ab48fd52e9dbf356aac211952a5
Peter Hellberg <peter@c7.se>
2018-03-09 18:41:34 +0100

    Log Authorization header

--- server/profile.go
+++ server/profile.go
@@ -3,6 +3,8 @@ package server
 import "net/http"
 
 func (s *Server) profile(w http.ResponseWriter, r *http.Request) {
+	s.log("Authorization: %#v", r.Header.Get("Authorization"))
+
 	meta := makeMeta(r, s.now())
 
 	writeJSON(w, Response{
